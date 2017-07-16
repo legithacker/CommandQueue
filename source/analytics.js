3b3d06e5461adae26235e93962f6fbef797a0d1f
mafrazz <mafrazzrafael@gmail.com>
2017-07-16 00:48:59 -0300

    [Analytics] Use module name replacer in ga id

--- source/analytics.js
+++ source/analytics.js
@@ -2,35 +2,35 @@ define('TWOverflow/Queue/analytics', [
     'TWOverflow/Queue'
 ], function (Queue) {
     Queue.analytics = function () {
-        ga('create', '__queue_analytics', 'auto', 'FarmOverflowQueue')
+        ga('create', '__queue_analytics', 'auto', '__queue_name')
 
         Queue.bind('start', function () {
-            ga('FarmOverflowQueue.send', 'event', 'behavior', 'start')
+            ga('__queue_name.send', 'event', 'behavior', 'start')
         })
 
         Queue.bind('stop', function () {
-            ga('FarmOverflowQueue.send', 'event', 'behavior', 'stop')
+            ga('__queue_name.send', 'event', 'behavior', 'stop')
         })
 
         Queue.bind('error', function (error) {
-            ga('FarmOverflowQueue.send', 'event', 'commands', 'error', error)
+            ga('__queue_name.send', 'event', 'commands', 'error', error)
         })
 
         Queue.bind('send', function (command) {
-            ga('FarmOverflowQueue.send', 'event', 'commands', 'send', command.type)
+            ga('__queue_name.send', 'event', 'commands', 'send', command.type)
         })
 
         Queue.bind('add', function () {
-            ga('FarmOverflowQueue.send', 'event', 'behavior', 'add')
+            ga('__queue_name.send', 'event', 'behavior', 'add')
         })
 
         Queue.bind('expired', function () {
-            ga('FarmOverflowQueue.send', 'event', 'commands', 'expired')
+            ga('__queue_name.send', 'event', 'commands', 'expired')
         })
 
         Queue.bind('remove', function (removed, command, manualRemove) {
             if (removed && manualRemove) {
-                ga('FarmOverflowQueue.send', 'event', 'commands', 'remove')
+                ga('__queue_name.send', 'event', 'commands', 'remove')
             }
         })
     }
